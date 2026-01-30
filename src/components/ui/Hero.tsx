df4666f636a4f4a69914d003d83ca4323f313960
Theres Östgård <theresb88@gmail.com>
2026-01-30 13:55:47 +0100

    Hero background.

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -1,14 +1,18 @@
 import { Link } from 'react-router-dom'
-
+import './hero.css'
+import bg from '../../assets/background-image.jpg'
 
 export default function Hero() {
   return (
     <section className="hero">
-      <h1 className="hero-title">Hello — I'm Theres</h1>
-      <p className="hero-sub">I build modern, accessible web experiences.</p>
-      <p>
-        <Link to="/projects" className="btn">View projects</Link>
-      </p>
+      <img src={bg} className="hero-bg" alt="Background" />
+      <div className="hero-inner">
+        <h1 className="hero-title">Hello — I'm Theres</h1>
+        <p className="hero-sub">I build modern, accessible web experiences.</p>
+        <p>
+          <Link to="/projects" className="btn">View projects</Link>
+        </p>
+      </div>
     </section>
   )
 }
